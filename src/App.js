0db49bc748d1c06d3445510992441171b1d693e3
Edgar <edgar.khojayan@livevegas.live>
2023-10-23 20:20:03 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="logo-container">
+      <span className="cut-wrapper">
+        <div class="circle-container">
+          <span className="border-bold">
+            <span className="chunks"></span>
+            <span className="logo-shadow"></span>
+          </span>
+        </div>
+        <span className="small-items"></span>
+
+        <img src={logo} alt="Live Vegas" className="logo-live" />
+      </span>
     </div>
   );
 }
