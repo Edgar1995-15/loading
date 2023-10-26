67ccc05dc8ad0ce42c75de3281271d1674a4f44d
Edgar <edgar.khojayan@livevegas.live>
2023-10-26 18:18:16 +0400

    fix(new)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import "./App.css";
-import Loading from "./Loading";
-// import logo from "./assets/logo.png";
+// import Loading from "./Loading";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
     <>
     
-    {/* <div className="logo-container"> */}
-      {/* <span className="cut-wrapper">
+    <div className="logo-container">
+      <span className="cut-wrapper">
         <div class="circle-container">
           <span className="chunks"></span>
+          <span className="chunks2"></span>
+          <span className="chunks3"></span>
+          <span className="chunks4"></span>
           <span className="border-bold">
             <span className="logo-shadow"></span>
           </span>
         </div>
         <img src={logo} alt="Live Vegas" className="logo-live" />
-      </span> */}
-      <Loading />
-    {/* </div> */}
+      </span>
+      {/* <Loading /> */}
+    </div>
     </>
   );
 }
