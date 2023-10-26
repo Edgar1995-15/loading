3946cbac595f0a1cd99750eca041c92d16e5f709
Edgar <edgar.khojayan@livevegas.live>
2023-10-26 19:55:50 +0400

    try fix own

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React, { useState } from 'react';
-
-import './App.css';
-import logo from './assets/logo1.png';
-import example from './assets/example-optimize.mp4';
+import "./App.css";
+// import Loading from "./Loading";
+// import logo from "./assets/logo1.png";
+import example from "./assets/11mp.mp4";
 
 function App() {
-  const [videoLoaded, setVideoLoaded] = useState(false);
-
-  const handleVideoLoad = () => {
-    setVideoLoaded(true);
-  };
-
   return (
-    <div className="logo-container">
-      <div className={`video-container ${videoLoaded ? 'show' : ''}`}>
-        <video
-          width="300"
-          height="300"
-          autoPlay
-          muted
-          playsInline
-          loop
-          onLoadedData={handleVideoLoad}
-          controls={false}
-          controlsList="nodownload"
-        >
-          <source src={example} type="video/mp4" />
-        </video>
-        <img src={logo} alt="Live Vegas" className={`logo-live ${videoLoaded ? 'show' : ''}`} />
+    <>
+      <div className="logo-container">
+            <div>
+              <video
+                width="300"
+                height="300"
+                autoPlay
+                muted
+                playsInline
+                loop
+                controls={false}
+                controlsList="nodownload"
+              >
+                <source src={example} type="video/mp4" />
+              </video>
+            </div>
+            {/* <span className="chunks"></span>
+          <span className="chunks2"></span>
+          <span className="chunks3"></span>
+          <span className="chunks4"></span>
+          <span className="border-bold">
+            <span className="logo-shadow"></span>
+          </span> */}
+          {/* <img src={logo} alt="Live Vegas" className="logo-live" /> */}
+        {/* <Loading /> */}
       </div>
-    </div>
+    </>
   );
 }
 
