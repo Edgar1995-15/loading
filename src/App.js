a7da67b6cf8c6815256c54b8133377f06911e7fd
Edgar <edgar.khojayan@livevegas.live>
2023-10-26 19:37:48 +0400

    try fix

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import "./App.css";
-// import Loading from "./Loading";
-import logo from "./assets/logo1.png";
-import example from "./assets/example-optimize.mp4";
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+import logo from './assets/logo1.png';
+import example from './assets/example-optimize.mp4';
 
 function App() {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
+  useEffect(() => {
+    const videoElement = document.getElementById('exampleVideo');
+
+    videoElement.addEventListener('loadeddata', () => {
+      setVideoLoaded(true);
+    });
+  }, []);
+
   return (
-    <>
-      <div className="logo-container">
-            <div>
-              <video
-                width="300"
-                height="300"
-                autoPlay
-                muted
-                playsInline
-                loop
-                controls={false}
-                controlsList="nodownload"
-              >
-                <source src={example} type="video/mp4" />
-              </video>
-            </div>
-            {/* <span className="chunks"></span>
-          <span className="chunks2"></span>
-          <span className="chunks3"></span>
-          <span className="chunks4"></span>
-          <span className="border-bold">
-            <span className="logo-shadow"></span>
-          </span> */}
+    <div className="logo-container">
+      <div>
+        <video
+          id="exampleVideo"
+          width="300"
+          height="300"
+          autoPlay
+          muted
+          playsInline
+          loop
+          controls={false}
+          controlsList="nodownload"
+        >
+          <source src={example} type="video/mp4" />
+        </video>
+        {videoLoaded && (
           <img src={logo} alt="Live Vegas" className="logo-live" />
-        {/* <Loading /> */}
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
