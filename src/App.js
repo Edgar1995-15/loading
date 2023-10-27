17d1137b2f266347c4eb6b2500f392aa156cb145
Edgar <edgar.khojayan@livevegas.live>
2023-10-27 18:30:34 +0400

    fix triangle

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ function App() {
         <span className="cut-wrapper">
           <div class="circle-container">
             <span className="chunks"></span>
+            <span className="chunks2"></span>
             <span className="insideCutter"></span>
             <span className="border-bold"></span>
+            <span className="border-shadow"></span>
+            <span className="border-shadow2"></span>
           </div>
           <img src={logo} alt="Live Vegas" className="logo-live" />
         </span>
